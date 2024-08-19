9cdfae7bfc850530ba0cd3fe436e7da92d441114
Shreya Banwaskar <shreyabanwaskar062@gmail.com>
2024-08-19 15:33:30 -0500

    Changes to the code

---
+++ index.js
@@ -0,0 +1,11 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
